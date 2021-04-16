74f8886abab9acca293a6ec1d65c23415c98c20a
Junior Martinez <jlmjunior@live.com>
2021-04-16 01:30:21 -0300

    Add project files.

---
+++ NUnitTDD.Test/CalculatorTest.cs
@@ -0,0 +1,33 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NUnitTDD.Test
+{
+    [TestFixture]
+    public class CalculatorTest
+    {
+        [Test]
+        public void MustAdd()
+        {
+            var sut = new Calculator();
+
+            var result = sut.Sum(15, 5);
+
+            Assert.That(result, Is.EqualTo(20));
+        }
+
+        [Test]
+        public void MustMultiply()
+        {
+            var sut = new Calculator();
+
+            var result = sut.Multiply(15, 5);
+
+            Assert.That(result, Is.EqualTo(75));
+        }
+    }
+}
---
+++ NUnitTDD/Calculator.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NUnitTDD
+{
+    public class Calculator
+    {
+        public int Sum(int num1, int num2)
+        {
+            return num1 + num2;
+        }
+
+        // Purposeful failure
+        public int Multiply(int num1, int num2)
+        {
+            // return num1 * num2;
+            return num1 + num2;
+        }
+    }
+}
